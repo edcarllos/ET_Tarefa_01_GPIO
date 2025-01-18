72175c6842a00dfe80bf30a76aa7c28f78656e1e
esantos <edcarllossantos@gmail.com>
2025-01-18 20:54:22 -0300

    Corrigindo arquivos básicos iniciais

---
+++ ET_Tarefa_01_GPIO.c
@@ -0,0 +1,23 @@
+/*
+ * Finalidade: Fazer o Buzzer tocar em uma frequência escolhida pelo teclado
+ *
+ */
+
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+
+#define GPIO_BUZZER 21
+#define GPIO_LED_R 13
+#define GPIO_LED_B 12
+#define GPIO_LED_G 11
+#define ASTERISTICO 13
+#define JOGO_DA_VELHA 14
+#define A 10
+#define B 11
+#define C 12
+#define D 15
+
+int main() {
+  return 0;
+}
